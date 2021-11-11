d0eb5ab78b5e4c351bcc7f32a2e5ef32442b7c02
Pol Nunez <polnunez@hotmail.es>
2021-11-11 22:55:44 +0100

    Plantilla cambiar contrasena

---
+++ views/nuevaContrasena.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    include '../bootstrap/index.php';
+    ?>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <!-- Login Form -->
+            <form action="">
+                <div class="modal-header">
+                    <h5 class="modal-title">Cambiar Contraseña</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="form-group">
+                <label for="Username">Contraseña</label>
+
+                    <div class="input-group mb-3" id="show_hide_password">
+                    
+                        <input class="form-control" type="password" aria-label="Amount (to the nearest dollar)">
+                        <span class="input-group-text"><a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a></span>
+                    </div>
+                </div>
+                <<div class="form-group">
+                <label for="Username">Nueva Contraseña</label>
+
+                    <div class="input-group mb-3" id="show_hide_password">
+                    
+                        <input class="form-control" type="password" aria-label="Amount (to the nearest dollar)">
+                        <span class="input-group-text"><a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a></span>
+                    </div>
+                    
+               
+                </div>
+                <div class="form-group">
+                <label for="Username">Repite Contraseña</label>
+
+                    <div class="input-group mb-3" id="show_hide_password">
+                    
+                        <input class="form-control" type="password" aria-label="Amount (to the nearest dollar)">
+                        <span class="input-group-text"><a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a></span>
+                    </div>
+                    
+               
+                </div>
+            </form>
+        </div>
+    </div>
+    <script src="https://kit.fontawesome.com/7fae944b38.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
--- views/tablaUsuarios.php
+++ views/tablaUsuarios.php
@@ -10,11 +10,6 @@
     include '../bootstrap/index.php';
     ?>
     <link rel="stylesheet" href="../styles/sass/administracion/dashboard/estiloDashboard.css">
-      <!-- Icons font CSS-->
-      <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <!-- Font special for pages-->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
 
 </head>
 
