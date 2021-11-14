a45620dbc3f0bf97dce3f24b68ae77f511d32bcb
Pol Nunez <polnunez@hotmail.es>
2021-11-14 23:02:08 +0100

    Botones profesiones

---
+++ views/seleccionarCiclo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    include '../bootstrap/index.php';
+    ?>
+    <link rel="stylesheet" type="text/css" href="../styles/css/style.css">
+
+</head>
+
+<body>
+<div class="container-fluid h-100"> 
+    		<div class="row w-100 align-items-center">
+    			<div class="col text-center  ">
+                <button class="btn btn-danger regular-button" id="marqueting" value="marqueting">Comerç i Màrqueting</button>
+                <button class="btn btn-danger regular-button" id="hoteleria" value="hoteleria">Hoteleria i Turisme </button>
+                <button class="btn btn-danger regular-button" id="informatica" value="informatica"> Informàtica i Comunicacions</button>
+                <button class="btn btn-danger regular-button" id="administracio "value="administracio"> Administració i Gestió</button>
+    			</div>	
+    		</div>
+    
+    	</div>
+
+</body>
+
+</html>
